c7a4349baa6b18ce0d797814bfb9740d414b45c3
Luis Steinert <76253309+luistnrt@users.noreply.github.com>
2023-07-24 13:49:10 +0200

    Update custom_environment.py and train_single_stock.ipynb

---
+++ custom_environment.py
@@ -0,0 +1,124 @@
+import gym
+import numpy as np
+from gym import spaces
+
+# Initialize a custom environment which we downlaoded from github.
+# Tradin Env:
+class CustomStockTradingEnv(gym.Env):
+    def __init__(self, df, window_size=10, k=1000, num_features=6, starting_balance=100000):
+        '''
+        df: dataframe of stock prices
+        window_size: number of previous days to consider
+        k: max number of shares to buy or sell
+        num_features: number of features to consider (i.e. Number of columns in dataframe not including date)
+        starting_balance: starting balance of account - the higher this is, the more leeway the agent has to make mistakes and learn
+        __init__ should initialize the action space and observation space        
+        '''
+        self.df = df
+        self.window_size = window_size
+        self.k = k
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-k, high=k, shape=(window_size, num_features), dtype=np.float32)
+        self.starting_balance = starting_balance
+        self.reset()
+    
+    def reset(self):
+        '''
+        Include the following lists over the training period:
+        self.profits: list of profits at each step
+        self.total_profits: list of cumulative profits at each step
+        self.shares: list of number of shares owned at each step
+        self.account_balance: list of account balance at each step
+        self.rewards: list of rewards at each step
+        etc
+        '''
+        self.current_step = self.window_size
+        self.total_portfolio_value = [self.starting_balance] # This is the account balance + value of shares owned at each iteration
+        self.account_balance = [self.starting_balance]
+        self.rewards = []
+        self.num_shares = [0] # This is the total number of shares owned for the stock for each iteration
+        self.trades = 0 # Extra variable to track number of trades
+
+        # Technical indicators
+        self.prices = self.df['Close'].values
+        self.macd = self.df['MACD'].values
+        self.signal = self.df['Signal'].values
+        self.rsi = self.df['RSI'].values
+        self.cci = self.df['CCI'].values
+        self.adx = self.df['ADX'].values
+        return self._next_observation()
+    
+    def _next_observation(self):
+        # Shape of obs: (window_size, num_features)
+        obs = np.array([
+            self.prices[(self.current_step - self.window_size):self.current_step],
+            self.macd[(self.current_step - self.window_size):self.current_step],
+            self.signal[(self.current_step - self.window_size):self.current_step],
+            self.rsi[(self.current_step - self.window_size):self.current_step],
+            self.cci[(self.current_step - self.window_size):self.current_step],
+            self.adx[(self.current_step - self.window_size):self.current_step]
+        ])
+        return np.transpose(obs)
+    
+    def step(self, action):
+        self._take_action(action)
+
+        self.current_step += 1
+
+        if self.current_step >= len(self.df) - 1:
+            done = True
+        else:
+            done = False
+
+        obs = self._next_observation()
+        return obs, sum(self.rewards), done, {"account_balance": self.account_balance, "num_shares": self.num_shares, "total_portfolio_value": self.total_portfolio_value}
+    
+    def calculate_reward(self):
+        '''
+        Calculate reward based on two factors:
+        1. account_balance[-1] - account_balance[-2]
+            * This includes the profit made upon selling some shares
+        2. Current value of the portfolio (i.e. num_shares * current_price)
+        '''
+        raw_reward = self.total_portfolio_value[-1] - self.total_portfolio_value[-2]
+        reward = np.clip(raw_reward, -1, 1) # Clip reward to be between -1 and 1
+        return reward
+    
+    def _take_action(self, action):
+        current_price = self._get_current_price() # The closing price for the day
+        action = action[0] # Should be a float between -1 and 1
+
+        # Buy if action > 0 and if you have enough money
+        if action > 0 and self.account_balance[-1] > current_price:
+            # Convert float to number of shares based on set "k" value (max number of shares to buy)
+            # If you don't have enough money, just buy as many as you can
+            shares_bought = min(int(self.account_balance[-1] / current_price), int(action * self.k))
+            self.account_balance.append(self.account_balance[-1] - shares_bought * current_price)
+            self.num_shares.append(self.num_shares[-1] + shares_bought)
+            self.total_portfolio_value.append(self.account_balance[-1] + self.num_shares[-1] * current_price)
+            self.trades += 1
+            self.rewards.append(self.calculate_reward())
+            
+        # Sell if action < 0 and if you have enough shares
+        elif action < 0 and self.num_shares[-1] > 0:
+            # Convert float to number of shares based on set "k" value (max number of shares to sell)
+            # If you don't have enough shares, just sell as many as you can
+            shares_sold = min(self.num_shares[-1], int(-action * self.k))
+            self.account_balance.append(self.account_balance[-1] + shares_sold * current_price)
+            self.num_shares.append(self.num_shares[-1] - shares_sold)
+            self.total_portfolio_value.append(self.account_balance[-1] + self.num_shares[-1] * current_price)
+            self.trades += 1
+            self.rewards.append(self.calculate_reward())
+        
+        # Otherwise, just hold
+        else:
+            self.account_balance.append(self.account_balance[-1])
+            self.num_shares.append(self.num_shares[-1])
+            self.total_portfolio_value.append(self.account_balance[-1] + self.num_shares[-1] * current_price)
+            self.rewards.append(self.calculate_reward())
+
+    def _get_current_price(self):
+        '''
+        Return the closing price for the current day
+        '''
+        return self.prices[self.current_step]     
---
+++ data_extractor.py
@@ -0,0 +1,173 @@
+import datetime
+import os
+import requests
+import pandas as pd
+import yfinance as yf
+
+
+def get_stock_names():
+    '''
+    Get the needed Stocks
+    '''
+    stock_names = ['APPL']
+    return stock_names
+
+
+def calculate_technical_indicators(data):
+    '''
+    Given a dataframe of stock data, calculate the following technical indicators:
+    Simple Moving Average (SMA)
+    Exponential Moving Average (EMA)
+    Moving Average Convergence Divergence (MACD)
+    Relative Strength Index (RSI)
+    Commodity Channel Index (CCI)
+    Average Directional Index (ADX)
+    '''
+    # Calculate SMA with a period of 10
+    sma = data["Close"].rolling(window=10).mean()
+
+    # Calculate EMA with a period of 10
+    ema = data["Close"].ewm(span=10, adjust=False).mean()
+
+    # Calculate MACD with fast period of 12, slow period of 26, and signal period of 9
+    ema_12 = data["Close"].ewm(span=12, adjust=False).mean()
+    ema_26 = data["Close"].ewm(span=26, adjust=False).mean()
+    macd = ema_12 - ema_26
+    signal = macd.ewm(span=9, adjust=False).mean()
+    macdhist = macd - signal
+
+    # Calculate RSI with a period of 14
+    delta = data["Close"].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=14).mean()
+    avg_loss = loss.rolling(window=14).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+
+    # Calculate the CCI with a period of 20
+    typical_price = (data["High"] + data["Low"] + data["Close"]) / 3
+    cci = typical_price - typical_price.rolling(window=20).mean()
+    cci /= 0.015 * typical_price.rolling(window=20).std()
+
+    # Calculate the ADX
+    tr = data["High"] - data["Low"]
+    tr1 = data["High"] - data["Close"].shift(1)
+    tr2 = data["Low"] - data["Close"].shift(1)
+    tr = tr.combine(tr1, max)
+    tr = tr.combine(tr2, max)
+    tr = tr.fillna(0)
+    tr_sum = tr.rolling(window=14).sum()
+    tr_sum = tr_sum.fillna(0)
+    dm_plus = data["High"] - data["High"].shift(1)
+    dm_minus = data["Low"].shift(1) - data["Low"]
+    dm_plus = dm_plus.where((dm_plus > 0) & (dm_plus > dm_minus), 0)
+    dm_minus = dm_minus.where((dm_minus > 0) & (dm_minus > dm_plus), 0)
+    dm_plus = dm_plus.fillna(0)
+    dm_minus = dm_minus.fillna(0)
+    dm_plus_ewm = dm_plus.ewm(span=14, adjust=False).mean()
+    dm_minus_ewm = dm_minus.ewm(span=14, adjust=False).mean()
+    di_plus = 100 * dm_plus_ewm / tr_sum
+    di_minus = 100 * dm_minus_ewm / tr_sum
+    dx = 100 * abs(di_plus - di_minus) / (di_plus + di_minus)
+    adx = dx.ewm(span=14, adjust=False).mean()
+
+    # Append the SMA, EMA, MACD, and RSI to the data
+    data["SMA"] = sma
+    data["EMA"] = ema
+    data["MACD"] = macd
+    data["Signal"] = signal
+    data["MACD_Hist"] = macdhist
+    data["RSI"] = rsi
+    data["CCI"] = cci
+    data["ADX"] = adx
+
+    return data
+
+
+def dataset_downloader(stock_name):
+    '''
+    Downloads stock data.
+    '''
+    # Replace all . with - in stock name (e.g. BRK.B -> BRK-B)
+    stock_name = stock_name.replace('.', '-')
+    data = yf.download(stock_name, progress=False) # progress=false to avoid printing progress bar
+
+    # Calculate technical indicators
+    data = calculate_technical_indicators(data)
+
+    return data
+
+
+def create_dataset(stock_names, data_folder):
+    '''
+    Given an array of stock tickers, create a csv for the last 5 years of data
+    for each stock and save it the data folder.
+    '''
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+
+    stock_count = 0
+    for stock_name in stock_names:
+        stock_count += 1
+        try:
+            print("Downloading data for " + stock_name + " [" + str(stock_count) + "/" + str(len(stock_names)) + "]")
+            data = dataset_downloader(stock_name)
+            data.to_csv(data_folder + stock_name + '.csv')
+        except Exception as e:
+            print(e)
+
+
+def dataloader(stock_name, data_folder, start_date, end_date):
+    '''
+    Given a stock ticker, data folder, start date and end date, load the data
+    from the csv file and return it as a pandas dataframe.
+    It loads data from start_date to end_date inclusive, unless end_date is past the latest date.
+    '''
+    with open(data_folder + stock_name + '.csv', mode='r') as f:
+        data = pd.read_csv(f)
+        data = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)]
+        return data
+
+
+def label_buy_sell_hold(data, threshold=0.02):
+    '''
+    Given a column of values, calculate for each value the average of the next 5 values (1 week).
+    If the value is [threshold] greater than the current price, append 1 for buy to the labels array.
+    If the value is [threshold] less than the current price, append 2 for sell to the labels array.
+    Otherwise, append 0 for hold.
+    You can change the threshold to fit your trading risk tolerance.
+    '''
+    # We only want the first column of values (Close price), but only if it's a 2D array
+    if len(data.shape) == 2:
+        data = data[:, 0]
+
+    # Convert data from (length, 1) to (length,)
+    data = data.reshape(-1)
+    labels = []
+    for i in range(len(data)):
+        if i + 5 < len(data):
+            avg = data[i+1:i+6].mean()
+            if avg > data[i] * 1 + threshold:
+                labels.append(1)
+            elif avg < data[i] * 1 - threshold:
+                labels.append(2)
+            else:
+                labels.append(0)
+        else: # If the last 5 values are not available, append whatever the last value is
+            labels.append(labels[-1])
+    
+    return labels # Labels is a list of 0, 1 or 2
+
+
+if __name__ == "__main__":
+    stock_names = get_stock_names()
+    # print(len(stock_names))
+    print(stock_names)
+
+    # This will download the entire history for each stock and save it to the data folder
+    create_dataset(stock_names, 'data/')
+
+    # This is how you load the data
+    data = dataloader('AAPL', 'data/', '2023-03-07', '2023-03-10')
+    print(data)
